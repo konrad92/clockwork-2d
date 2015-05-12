823574863d933b7c7d3a59d83d4d3471cbdc7ade
Konrad Nowakowski <konrad.x92@gmail.com>
2015-05-12 21:55:59 +0200

    Dodałem papierową kulkę. Na razie bez tekstury.

---
+++ src/vault/clockwork/actors/PaperBallActor.java
@@ -0,0 +1,164 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Konrad Nowakowski https://github.com/konrad92.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package vault.clockwork.actors;
+
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import vault.clockwork.Game;
+import vault.clockwork.scene.Actor;
+import vault.clockwork.system.Physics;
+
+/**
+ *
+ * @author Konrad Nowakowski https://github.com/konrad92
+ */
+public class PaperBallActor extends ObstacleActor {
+	/**
+	 * Physics body.
+	 */
+	private final Body body;
+	
+	/**
+	 * The body single fixture.
+	 */
+	private final Fixture fixture;
+	
+	/**
+	 * Paper ball sprite to be drawn.
+	 */
+	private final Sprite sprBall;
+	
+	/**
+	 * Ctor.
+	 * Create new physic body on the world.
+	 * @see Actor#Actor(int) 
+	 * @param id Turret unique id.
+	 */
+	public PaperBallActor(int id) {
+		super(id);
+		
+		// body shape
+		CircleShape shape = new CircleShape();
+		shape.setRadius(32.f * Physics.SCALE);
+		
+		// create physics body
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set((float)Math.random()*1.f, (float)Math.random()*1.f);
+		body = Game.physics.world.createBody(bodyDef);
+		
+		// make the fixtures
+		fixture = body.createFixture(shape, 2.f);
+		fixture.setRestitution(.4f);
+		fixture.setFriction(.5f);
+		fixture.setUserData(this);
+		
+		shape.dispose();
+		
+		// create the ball sprite
+		sprBall = new Sprite(Game.assets.get("assets/dragonball.png", Texture.class));
+		sprBall.setBounds(-42.f, -42.f, 84.f, 84.f);
+		sprBall.setOriginCenter();
+		
+		// dodanie dzwiekow do odegrania
+		impactSounds.addAll(
+			Game.assets.get("assets/sounds/paperhit.ogg", Sound.class)
+		);
+	}
+	
+	/**
+	 * @see Actor#update(float) 
+	 * @param delta 
+	 */
+	@Override
+	public void update(float delta) {
+	}
+	
+	/**
+	 * @see Actor#draw(com.badlogic.gdx.graphics.g2d.SpriteBatch) 
+	 * @param batch 
+	 */
+	@Override
+	public void draw(SpriteBatch batch) {
+		sprBall.setCenter(
+			body.getPosition().x * Physics.SCALE_INV,
+			body.getPosition().y * Physics.SCALE_INV
+		);
+		
+		batch.begin();
+		sprBall.draw(batch);
+		batch.end();
+	}
+	
+	/**
+	 * Remove actor on impact with WielokatActor.
+	 * @param actor
+	 * @param contact 
+	 */
+	@Override
+	public void onHit(Actor actor, Contact contact) {
+		if(actor instanceof WielokatActor || actor instanceof DustbinActor) {
+			this.remove();
+		}
+		
+		if(actor instanceof ObstacleActor){
+			((ObstacleActor)actor).playImpactSound();
+		}
+	}
+	
+	/**
+	 * Remove physic body from the world.
+	 * @see Actor#dispose() 
+	 */
+	@Override
+	public void dispose() {
+		Game.physics.world.destroyBody(body);
+	}
+	
+	/**
+	 * @see Actor#getPosition() 
+	 * @return 
+	 */
+	@Override
+	public Vector2 getPosition() {
+		return body.getTransform().getPosition().scl(Physics.SCALE_INV);
+	}
+	
+	/**
+	 * @see Actor#setPosition(com.badlogic.gdx.math.Vector2) 
+	 * @param newPosition 
+	 */
+	@Override
+	public void setPosition(Vector2 newPosition) {
+		body.setTransform(newPosition.cpy().scl(Physics.SCALE), body.getTransform().getRotation());
+	}
+}
